fcd21f021ae6c449acb9bd39b5ac3faae0e122f2
Quan <tennam454@gmail.com>
2025-01-10 23:06:35 +0700

    kiemtra

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "/products", to: "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
+  resource :unsubscribe, only: [ :show ]
+
+
   root "products#index"
 
 end
